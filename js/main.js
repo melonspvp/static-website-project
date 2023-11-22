8df5a38fe4c328958aebd2e69d70ebcd78056c7e
João Marcelo Hilbert Mello <melonsfort@gmail.com>
2023-11-21 22:05:07 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,123 @@
+function toggleMenu() {
+  var menu = document.getElementById("menu-sobreposto");
+  menu.classList.remove("menu-sobreposto-close");
+  menu.classList.add("menu-sobreposto");
+  menu.style.display = menu.style.display = "block";
+  var item1 = document.getElementById("imagem1porra");
+  item1.style.display = "flex";
+}
+
+function closeMenu() {
+  var close = document.getElementById("menu-sobreposto");
+  close.classList.remove("menu-sobreposto");
+  close.classList.add("menu-sobreposto-close");
+  close.style.display = "none";
+  var item1 = document.getElementById("imagem1porra");
+  item1.style.display = "flex";
+  var item2 = document.getElementById("imagem2porra");
+  item2.style.display = "none";
+  var item3 = document.getElementById("imagem3porra");
+  item3.style.display = "none";
+  var item4 = document.getElementById("imagem4porra");
+  item4.style.display = "none";
+  var item5 = document.getElementById("imagem5porra");
+  item5.style.display = "none";
+  var item6 = document.getElementById("imagem6porra");
+  item6.style.display = "none";
+  //   setTimeout(function () {
+  // close.style.display = "none";
+  //   }, 10);
+  console.log(close.classList);
+}
+
+function menuItem1() {
+  var item1 = document.getElementById("imagem1porra");
+  item1.style.display = "flex";
+  var item2 = document.getElementById("imagem2porra");
+  item2.style.display = "none";
+  var item3 = document.getElementById("imagem3porra");
+  item3.style.display = "none";
+  var item4 = document.getElementById("imagem4porra");
+  item4.style.display = "none";
+  var item5 = document.getElementById("imagem5porra");
+  item5.style.display = "none";
+  var item6 = document.getElementById("imagem6porra");
+  item6.style.display = "none";
+}
+
+function menuItem2() {
+  var item1 = document.getElementById("imagem1porra");
+  item1.style.display = "none";
+  var item2 = document.getElementById("imagem2porra");
+  item2.style.display = "flex";
+  var item3 = document.getElementById("imagem3porra");
+  item3.style.display = "none";
+  var item4 = document.getElementById("imagem4porra");
+  item4.style.display = "none";
+  var item5 = document.getElementById("imagem5porra");
+  item5.style.display = "none";
+  var item6 = document.getElementById("imagem6porra");
+  item6.style.display = "none";
+}
+
+function menuItem3() {
+  var item1 = document.getElementById("imagem1porra");
+  item1.style.display = "none";
+  var item2 = document.getElementById("imagem2porra");
+  item2.style.display = "none";
+  var item3 = document.getElementById("imagem3porra");
+  item3.style.display = "flex";
+  var item4 = document.getElementById("imagem4porra");
+  item4.style.display = "none";
+  var item5 = document.getElementById("imagem5porra");
+  item5.style.display = "none";
+  var item6 = document.getElementById("imagem6porra");
+  item6.style.display = "none";
+}
+
+function menuItem4() {
+  var item1 = document.getElementById("imagem1porra");
+  item1.style.display = "none";
+  var item2 = document.getElementById("imagem2porra");
+  item2.style.display = "none";
+  var item3 = document.getElementById("imagem3porra");
+  item3.style.display = "none";
+  var item4 = document.getElementById("imagem4porra");
+  item4.style.display = "flex";
+  var item5 = document.getElementById("imagem5porra");
+  item5.style.display = "none";
+  var item6 = document.getElementById("imagem6porra");
+  item6.style.display = "none";
+}
+
+function menuItem5() {
+  var item1 = document.getElementById("imagem1porra");
+  item1.style.display = "none";
+  var item2 = document.getElementById("imagem2porra");
+  item2.style.display = "none";
+  var item3 = document.getElementById("imagem3porra");
+  item3.style.display = "none";
+  var item4 = document.getElementById("imagem4porra");
+  item4.style.display = "none";
+  var item5 = document.getElementById("imagem5porra");
+  item5.style.display = "flex";
+  var item6 = document.getElementById("imagem6porra");
+  item6.style.display = "none";
+}
+
+function menuItem6() {
+  var item1 = document.getElementById("imagem1porra");
+  item1.style.display = "none";
+  var item2 = document.getElementById("imagem2porra");
+  item2.style.display = "none";
+  var item3 = document.getElementById("imagem3porra");
+  item3.style.display = "none";
+  var item4 = document.getElementById("imagem4porra");
+  item4.style.display = "none";
+  var item5 = document.getElementById("imagem5porra");
+  item5.style.display = "none";
+  var item6 = document.getElementById("imagem6porra");
+  item6.style.display = "flex";
+}
+    
+  
