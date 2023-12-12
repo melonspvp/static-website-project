d81ddbb63a221aa18c00bcb472ef697559ab82ae
João Marcelo Hilbert Mello <melonsfort@gmail.com>
2023-12-11 21:16:34 -0300

    Shapes page is done. Header and footer are done. Now the home page is in progress.

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-This is a project for a static sales website related to Skateboarding.
-So far it has 3 pages, but more will be added at the end of the project.
-
-Author: João Marcelo
+# projeto-ali-outro
---
+++ js/filterShape.js
@@ -0,0 +1,26 @@
+function expandFilter1() {
+  let filter1 = document.getElementById("filter1");
+  filter1.style.display =
+    filter1.style.display === "flex" || filter1.style.display === ""
+      ? "none"
+      : "flex";
+  console.log(filter1.style.display);
+}
+
+function expandFilter2() {
+  let filter2 = document.getElementById("filter2");
+  filter2.style.display =
+    filter2.style.display === "flex" || filter2.style.display === ""
+      ? "none"
+      : "flex";
+  console.log(filter2.style.display);
+}
+
+function expandFilter3() {
+  let filter3 = document.getElementById("filter3");
+  filter3.style.display =
+    filter3.style.display === "flex" || filter3.style.display === ""
+      ? "none"
+      : "flex";
+  console.log(filter3.style.display);
+}
